4ac5b50cdd1edb1272c70ad37d4802369f174023
jina <bluerose021318@gmail.com>
2020-08-18 21:41:37 +0900

    Modify: 초기세팅 page/styles 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Main from "./Pages/Main";
+import SignUp from "./Pages/SignUp";
+import Shop from "./Pages/Shop";
+import ShopListCol from "./Pages/ShopListCol";
+import ShopListRow from "./Pages/ShopListRow";
+import ShowDetail from "./Pages/ShowDetail";
+import WishList from "./Pages/WishList";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Switch>{/* <Route exact path="/" component={Home} /> */}</Switch>
+        <Switch>
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/Shop" component={Shop} />
+          <Route exact path="/ShopListCol" component={ShopListCol} />
+          <Route exact path="/ShopListRow" component={ShopListRow} />
+          <Route exact path="/ShowDetail" component={ShowDetail} />
+          <Route exact path="/WishList" component={WishList} />
+        </Switch>
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-import "./index.css";
+import "./index.scss";
 import "./style/reset.css";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
