0945cb6ebd462e2441f8108b55cdcbebc191519c
Seob <seob_@kakao.com>
2020-08-19 22:30:43 +0900

    Add:제품상세페이지 레이아웃 추가 - 상세 사진 props로 받기 가능 - 옵션선택 기능 수정 필요 - 버튼 기능 구현 필요

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,55 @@
 import React, { Component } from "react";
+import Video from "../../Components/Video/Video";
+import "./Main.scss";
 
+const videos = {
+  Main: {
+    src:
+      "https://www.yuca.tv/custom/celine/player.php?uid=ebdbb4eb-4586-5873-abb3-aa161bf8b0b1&lang=en&type=standard&loop=true&mute=true&autorun=true&_=0.5787494838904113",
+    description: [
+      <span>
+        CELINE HOMME
+        <br />
+        THE DANCING KID
+        <br />
+        CIRCUIT DU CASTELLET, FRANCE
+        <br />
+        JULY 13TH 2020
+        <br />
+        <br />
+        MUSIC ORIGINAL SOUNDTRACK FOR CELINE
+        <br />
+        "THEY CALL ME TIAGO (THE DANCING KID CELINE EDIT)"
+        <br />
+        PERFORMED BY TIAGZ
+        <br />
+        COURTESY OF EPIC RECORDS BY ARRANGEMENT WITH SONY MUSIC ENTERTAINMENT
+        <br />
+        <br />
+        STYLING AND DIRECTING "THE DANCING KID"
+        <br />
+        HEDI SLIMANE
+        <br />
+        <br />
+        MAKE-UP ARTIST
+        <br />
+        CHRISTELLE COCQUET
+        <br />
+        <br />
+        HAIRSTYLISTS
+        <br />
+        ESTHER LANGHAM AND ALEX BROWNSELL <br />
+        <br />
+      </span>,
+    ],
+  },
+};
 export default class Main extends Component {
   render() {
-    return <div></div>;
+    return (
+      <main className="Main">
+        <Video videos={videos} />
+      </main>
+    );
   }
 }
--- src/Pages/Shop/ShopDetails.js
+++ src/Pages/Shop/ShopDetails.js
@@ -1,7 +1,113 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./ShopDetails.scss";
 
+const itemPics = [
+  "https://twicpics.celine.com/products/dw2a049ab4/images/large/339703117C.38NO_0_FALL20_742753v2.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  "https://twicpics.celine.com/products/dw208fcd48/images/large/339703117C.38NO_1_FALL20_742702v1.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  "https://twicpics.celine.com/products/dwcf9b0128/images/large/339703117C.38NO_2_FALL20_742795v1.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  "https://twicpics.celine.com/products/dw8d590640/images/large/339703117C.38NO_6_FALL20_742767v2.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  "https://twicpics.celine.com/products/dw0eaab394/images/large/339703117C.38NO_7_FALL20_721371v2.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+];
+
+const recentlyViewed = {
+  itemName1:
+    "https://twicpics.celine.com/products/dw1d90136d/images/large/339913190C.38NO_2_FALL20_741504v2.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  itemName2:
+    "https://twicpics.celine.com/products/dwd7bd6f4c/images/large/339183190C.38NO_1_FALL20_781755v1.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+  itemName3:
+    "https://twicpics.celine.com/products/dw94abfe9e/images/large/338343363C.04LU_2_SUM20_602203v2.jpg?sw=1000&sh=1000&sm=fit&strip=false",
+  itemName4:
+    "https://twicpics.celine.com/products/dw6d942286/images/large/338443433C.04LU_2_SUM20_800868v2.jpg?sw=1000&sh=1000&sm=fit&strip=false",
+  itemName5:
+    "https://twicpics.celine.com/products/dw257ee05e/images/large/336623363C.04LU_2_SPR20_408513v2.jpg?sw=1000&sh=1000&sm=fit&strip=false",
+  itemName6:
+    "https://twicpics.celine.com/products/dw2a049ab4/images/large/339703117C.38NO_0_FALL20_742753v2.jpg?sw=1900&sh=1900&sm=fit&strip=false",
+};
+
+const options = {
+  Black:
+    "https://www.celine.com/on/demandware.static/-/Sites-masterCatalog/default/dw8a555928/images/swatch/38NO.jpg",
+  option2: "option2",
+  option3: "option3",
+  option4: "option4",
+};
 export default class ShopDetails extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="ShopDetails">
+        <div className="detailWrapper">
+          <div className="returnToListing">
+            <Link to="/">return to listing</Link>
+          </div>
+          <div className="detailContainer">
+            <div className="detailedPictures">
+              <ul>
+                {itemPics.map((picture) => (
+                  <li className={itemPics.length % 2 ? "oddList" : "evenList"}>
+                    <img src={picture} alt="" />
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="itemDescriptions">
+              <h1>CHAT BOTTÉ OVER-THE-KNEE FLAT BOOT IN SUEDE CALFSKIN</h1>
+              <p>
+                100% Calfskin
+                <br />
+                0.2 IN (5 MM) heel
+                <br />
+                Elongated almond toe shape
+                <br />
+                Decorative laces with dangling tassels
+                <br />
+                Slip on
+                <br />
+                Leather insole
+                <br />
+                Leather thin outsole
+                <br />
+                Reference : 339703117C.38NO
+              </p>
+              <div className="optionWrapper">
+                <div className="option">
+                  <div className="optionLabel">
+                    colors
+                    <select name="option">
+                      {Object.keys(options).map((option, idx) => (
+                        <option key={idx} value={option}>
+                          {options[option]}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="contactUs">contact us</div>
+                  <div className="addToWishlist">add to wishlist</div>
+                  <div className="share">share</div>
+                </div>
+                <button>find in store</button>
+              </div>
+            </div>
+          </div>
+          <div className="recentlyViewed">
+            <h2>recently viewed</h2>
+            <ul>
+              {Object.keys(recentlyViewed).map((item, idx) => (
+                <li>
+                  <img key={idx} src={recentlyViewed[item]} alt={item} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <nav className="itemDirectory">
+            <ul>
+              <li>
+                <Link to="/">Celine</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
   }
 }
