54177f7d8d5d941aa4ead6ff2591cf56cb458d9e
Doyoonear <doyoonleee@gmail.com>
2020-08-19 23:12:59 +0900

    Add: 자동 scroll 기능 구현(다른 방식으로 수정 예정)

--- src/Pages/Shop/ShopListRowItems.js
+++ src/Pages/Shop/ShopListRowItems.js
@@ -3,7 +3,22 @@ import React, { Component } from "react";
 import ShopListRowItem from "./ShopListRowItem";
 
 export default class ShopListRowItems extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scroll: 0,
+    };
+    this.ulRef = React.createRef();
+  }
+  runScroll = (e) => {
+    console.log("runScroll");
+    document.getElementsByClassName("itemList")[0].scrollTo({
+      left: 3000,
+      behavior: "smooth",
+    });
+  };
   render() {
+    console.log(this.ulRef);
     return (
       <div className="shopListRowItems">
         <div className="wholeBox">
@@ -16,7 +31,11 @@ export default class ShopListRowItems extends Component {
                 <span>VIEW ALL</span>
               </a>
             </div>
-            <ul className="itemList">
+            <ul
+              ref={this.ulRef}
+              className="itemList"
+              onMouseEnter={this.runScroll}
+            >
               {this.props.itemList.list.map((el) => {
                 return <ShopListRowItem item={el} />;
               })}
