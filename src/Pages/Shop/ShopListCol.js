f9e9c6e9a736aaa5797b830e9104086053f4b36e
한태규 <teagyu1058@gmail.com>
2020-08-20 12:08:24 +0900

    Add:수직 상품 리스트 추가

--- src/Pages/Shop/ShopListCol.js
+++ src/Pages/Shop/ShopListCol.js
@@ -1,7 +1,72 @@
-import React, { Component } from "react";
+import React from "react";
+import GoodsList from "../../Components/GoodsList";
+import "./ShopListCol.scss";
+import "../../Styles/common.scss";
 
-export default class ShopListCol extends Component {
+class ShopListCol extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mouseover: true,
+      Goods: [
+        {
+          category: "A",
+          List: [
+            {
+              key: "1",
+              name: "CUBAINE MEDIUM BOOT IN CALFSKIN",
+              src: "./Images/images.jpeg",
+              src2: "./Images/images1.jpeg",
+              new: true,
+              href: "",
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  /*componentDidMount = () => {
+    fetch("주소", {
+      method: "GET",
+      body: JSON.stringify({}),
+    })
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };*/
+
+  HoverImgChanger = () => {
+    this.setState({
+      mouseover: !this.state.mouseover,
+    });
+  };
+
   render() {
-    return <div></div>;
+    console.log(this.state);
+    return (
+      <div className="ShopListCol">
+        <aside></aside>
+        <main>
+          <header>
+            <span>BOOTS</span>
+          </header>
+          <GoodsList
+            Goods={this.state.Goods}
+            mouseover={this.state.mouseover}
+          />
+          <nav>
+            <a href="?">WELINE</a>
+            <span>/</span>
+            <a href="?">WELINE WOMEN</a>
+            <span>/</span>
+            <a href="?">SHOES</a>
+            <span>/</span>
+            <span>BOOTS</span>
+          </nav>
+        </main>
+      </div>
+    );
   }
 }
+
+export default ShopListCol;
