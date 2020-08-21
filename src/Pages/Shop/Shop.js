c2e056867ea221eebc372438543f3e99f9876cd1
한태규 <teagyu1058@gmail.com>
2020-08-21 11:51:47 +0900

    Modify: 중간세이브

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -3,85 +3,36 @@ import "../../Styles/common.scss";
 import "./Shop.scss";
 
 class Shop extends Component {
+  constructor() {
+    super();
+    this.state = {
+      accordion: [],
+    };
+  }
+
+  componentDidMount = () => {
+    fetch("/Data/MockData/shop.json")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ accordion: response.accordion });
+      });
+  };
   render() {
     return (
       <div className="Shop">
         <aside></aside>
         <main>
           <ul>
-            <li className="ACCESSORIES">
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dwae4f7a92/navigation-recast/16.WOMEN_WINTER_20_PART_2_SGL.jpg"
-                />
-                <span>ACCESSORIES</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dwe22fbd6b/navigation-recast/16.WOMEN_WINTER_20_PART_2_JWL.jpg"
-                />
-                <span>JEWELLERY</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dwc694a6c4/navigation-recast/16.WOMEN_WINTER_20_PART_2_SLG.jpg"
-                />
-                <span>SMALL LEATHER GOODS</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dw22f941c4/navigation-recast/16.WOMEN_WINTER_20_PART_2_BAGS.jpg"
-                />
-                <span>HANDBAGS</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dw58420ebe/navigation-recast/16.WOMEN_WINTER_20_PART_2_SHOES_STATIQUE.jpg"
-                  //src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dw21798fca/navigation-recast/16.WOMEN_WINTER_20_PART_2_SHOES.gif"
-                />
-                <span>SHOES</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dw3dbadd67/navigation-recast/16.WOMEN_WINTER_20_PART_2_RTW_02.jpg"
-                />
-                <span>READY TO WEAR</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dwaf401b49/navigation-recast/16.WOMEN_WINTER_20_PART_1_CHARMS.jpg"
-                />
-                <span>CHARMS COLLECTION</span>
-              </a>
-            </li>
-            <li>
-              <a href="?">
-                <img
-                  alt="manu pic"
-                  src="https://www.celine.com/on/demandware.static/-/Library-Sites-Celine-SharedLibrary/default/dw026a1b09/navigation-recast/16.WOMEN_WINTER_20_PART_2_NC.jpg"
-                />
-                <span>NEW WINTER PART 1</span>
-              </a>
-            </li>
+            {this.state.accordion.map((tab) => (
+              <li>
+                <a href={tab.href}>
+                  <img alt="manu pic" src={tab.src} />
+                  <span>{tab.text[0]}</span>
+                  <span>{tab.text[1]}</span>
+                  <span>{tab.text[2]}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </main>
       </div>
