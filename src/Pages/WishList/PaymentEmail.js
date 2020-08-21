7fde2972cfa737c6862dba4077f019f34282419c
Taegyu Han <65657197+recurian1058@users.noreply.github.com>
2020-08-21 15:32:46 +0900

    Update PaymentEmail.js

--- src/Pages/WishList/PaymentEmail.js
+++ src/Pages/WishList/PaymentEmail.js
@@ -14,7 +14,7 @@ class PaymentEmail extends React.Component {
   componentDidMount = () => {
     fetch("/data/mockData/Goods.json")
       .then((response) => response.json())
-      .then((Goods) => this.setState({ Goods }));
+      .then((response) => this.setState({ Goods: response.Goods }));
   };
 
   render() {
